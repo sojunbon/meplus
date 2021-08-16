a42693a7e57eda0e06553bbc8ddfe355be7c9a4f
sojunbon <57395101+sojunbon@users.noreply.github.com>
2021-08-16 20:52:50 +0700

    update meplus

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,8 @@ import 'package:meplus/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(FIrebaseAuthentication());
+    // await tester.pumpWidget(FIrebaseAuthentication());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
