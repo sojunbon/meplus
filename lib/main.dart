27dc2885af72f9b78e933175f9d5778f5c312aab
sojunbon <57395101+sojunbon@users.noreply.github.com>
2021-08-17 15:46:44 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'eCommerce int2',
+      title: 'ME PLUS',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.light,
